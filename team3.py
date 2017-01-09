30fed5deb1c3d5d0210096e14934dc02429953eb
aaa <aaa>
2017-01-09 12:48:13 -0500

    its the pacer test
    Just type paceretest() and see what happens ;)

--- team3.py
+++ team3.py
@@ -65,4 +65,7 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')  
+              
+def pacertest():
+    print 'The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute'         
